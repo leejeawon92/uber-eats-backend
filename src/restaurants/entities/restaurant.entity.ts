98829753780a7a4aa9f0f130e63e71ecf3bc3893
leejeawon92 <jeawon33333@gmail.com>
2024-03-28 11:01:30 +0900

    apollo server-ObjectTypes

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,12 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Restaurant { // graphQL관점에서 본 restaurant가 어떻게 생겼는지 묘사를 한 것
+  @Field(type => String)
+  name: string;
+
+  @Field(type => Boolean, { nullable: true })
+  isGood?: boolean;
+}
+
+// entities는 데이터베이스의 모델이라고 생각하면 된다
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,10 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantResolver {
-  @Query(returns => Boolean)
-  isPizzaGood(): Boolean {
+  @Query(returns => Restaurant) // Restaurant를 return하고 Restaurant는 restaurant.entity.ts파일에서 만든 Object Type의 모양대로 들어갈 것이다.
+  myRestaurant() {
     return true;
   }
 }
