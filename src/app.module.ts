5de728e4817d0213fe754b1adfc8eaca41480cc5
leejeawon92 <jeawon33333@gmail.com>
2024-03-31 10:44:27 +0900

    typeorm connection setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,7 +10,17 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RestaurantsModule
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'jeawon',
+      password: '12345',
+      database: 'uber-eats',
+      synchronize: true,
+      logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
