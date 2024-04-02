6df6820f9e4e49fb7e5854b3b53878e5f79f5c55
leejeawon92 <jeawon33333@gmail.com>
2024-04-02 10:04:14 +0900

    TypeORM-Entity used

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,18 +1,32 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant { // graphQL관점에서 본 restaurant가 어떻게 생겼는지 묘사를 한 것
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
   @Field(type => String)
+  @Column()
   name: string;
 
   @Field(type => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(type => String)
+  @Column()
   address: string;
 
   @Field(type => String)
+  @Column()
   ownersName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
 }
 
 // entities는 데이터베이스의 모델이라고 생각하면 된다
