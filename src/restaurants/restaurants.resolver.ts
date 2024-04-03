302ff8337ca917c0400847bb3ec503e1e60e657a
leejeawon92 <jeawon33333@gmail.com>
2024-04-03 11:24:21 +0900

    Active Record- Repository

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,27 +1,29 @@
 import { Query, Resolver,Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
-  // @Query(returns => Restaurant) // Restaurant를 return하고 Restaurant는 restaurant.entity.ts파일에서 만든 Object Type의 모양대로 들어갈 것이다.
-  // myRestaurant() {
-  //   return true;
-  // }
+  constructor(private readonly restaurantService: RestaurantService) {}
   @Query(returns => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] { // veganOnly라는 이름을 가진 argument를 요청한다. 그리고 veganOnly는 boolean값을 가진다
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
-
-  // @Mutation(returns => Boolean)
-  // createRestaurant(@Args('createRestaurantDto') createRestaurantInput: CreateRestaurantDto): boolean {
-  //   console.log(createRestaurantInput);
-  //   return true;
-  // }
-
+  
   @Mutation(returns => Boolean)
   createRestaurant(@Args('createRestaurantInput') createRestaurantDto: CreateRestaurantDto): boolean {
     console.log(createRestaurantDto);
     return true;
   }
+
+    // @Query(returns => Restaurant) // Restaurant를 return하고 Restaurant는 restaurant.entity.ts파일에서 만든 Object Type의 모양대로 들어갈 것이다.
+  // myRestaurant() {
+  //   return true;
+  // }
+  // @Mutation(returns => Boolean)
+  // createRestaurant(@Args('createRestaurantDto') createRestaurantInput: CreateRestaurantDto): boolean {
+  //   console.log(createRestaurantInput);
+  //   return true;
+  // }
 }
