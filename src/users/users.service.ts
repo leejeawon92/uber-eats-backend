e796901a4b1f6592e4b8e45d6103c31d8c719328
leejeawon92 <jeawon33333@gmail.com>
2024-04-12 12:02:23 +0900

    create account service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,9 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateAccountInput } from './dtos/create-account.dto';
 
 @Injectable()
 export class UserService {
   constructor( @InjectRepository(User) private readonly users: Repository<User>) {
+  }
+
+  async createAccount({ email, password, role }: CreateAccountInput) {
+    try {
+      const exists = await this.users.findOne({ where: { email } });
+      if (exists) {
+        return;
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return true;
+    } catch (e) {
+      return;
+    }
   }
 }
