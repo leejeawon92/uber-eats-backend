69021ed2efb2d6a9ff53bff1b54dba87d7729f09
leejeawon92 <jeawon33333@gmail.com>
2024-04-14 09:54:57 +0900

    Another way to handle errors

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,15 +9,16 @@ export class UserService {
   constructor( @InjectRepository(User) private readonly users: Repository<User>) {
   }
 
-  async createAccount({ email, password, role}: CreateAccountInput): Promise<string | undefined> {
+  async createAccount({ email, password, role}: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ where: { email } });
       if (exists) {
-        return '이미 이메일이 존재합니다.';
+        return { ok: false, error: '이미 이메일이 존재합니다.' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return "계정을 생성할 수 없습니다.";
+      return { ok: false, error: '계정을 생성할 수 없습니다.'};
     }
   }
 }
