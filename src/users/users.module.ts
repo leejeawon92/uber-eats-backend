9e4a69aa84824199ed6a5ceda9b6a210ff9947f8
leejeawon92 <jeawon33333@gmail.com>
2024-04-24 12:34:54 +0900

    In the header, the user creates middleware to send to the request

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,5 +9,6 @@ import { JwtService } from 'src/jwt/jwt.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserResolver, UserService],
+  exports: [UserService],
 })
 export class UsersModule {}
