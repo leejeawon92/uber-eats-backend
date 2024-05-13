24c111c330ad3103e3b67f0ab40c706972bf0c5a
leejeawon92 <jeawon33333@gmail.com>
2024-05-13 17:10:52 +0900

    Set up the mock type

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -5,6 +5,7 @@ import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(),
@@ -21,8 +22,11 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UserService;
+  let usersRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -47,13 +51,16 @@ describe('UserService', () => {
       ],
     }).compile();
     service = module.get<UserService>(UserService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    it('사용자가 있을 경우 실패해야한다', () => {});
+  });
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
