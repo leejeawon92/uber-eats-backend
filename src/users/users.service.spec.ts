1195330d74ecc873070c6b382b611393e3f3544a
leejeawon92 <jeawon33333@gmail.com>
2024-05-20 17:55:47 +0900

    editProfile-create user, emailservice

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -193,8 +193,47 @@ describe('UserService', () => {
     });
   });
 
+  describe('editProfile', () => {
+    it('이메일을 변경해야 한다.', async () => {
+      const oldUser = {
+        email: 'jeawon33333@naver.com',
+        verified: true,
+      };
+      const editProfileArgs = {
+        userId: 1,
+        input: { email: 'jeawon33333@naver.com' },
+      };
+      const newVerification = {
+        code: 'code',
+      };
+      const newUser = {
+        verified: false,
+        email: editProfileArgs.input.email,
+      };
 
-  it.todo('findById');
-  it.todo('editProfile');
+      usersRepository.findOne.mockResolvedValue(oldUser);
+      verificationsRepository.create.mockReturnValue(newVerification);
+      verificationsRepository.save.mockResolvedValue(newVerification);
+
+      await service.editProfile(editProfileArgs.userId, editProfileArgs.input);
+
+      expect(usersRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(usersRepository.findOne).toHaveBeenCalledWith({
+        where: { id: editProfileArgs.userId}
+      });
+
+      expect(verificationsRepository.create).toHaveBeenCalledWith({
+        user: newUser,
+      });
+      expect(verificationsRepository.save).toHaveBeenCalledWith(
+        newVerification,
+      );
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        newUser.email,
+        newVerification.code,
+      );
+    });
+  });
   it.todo('verifyEmail');
 });
