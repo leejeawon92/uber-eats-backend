077e26658aa881e4c92e33df90054e4c00a6ceb2
leejeawon92 <jeawon33333@gmail.com>
2024-05-24 17:23:02 +0900

    jwt sign test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+import * as jwt from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 
@@ -20,5 +27,15 @@ describe('JwtService', () => {
   });
   it('정의되어야 한다.', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('sign', () => {
+    it('토큰을 반환해야 한다.', () => {
+      const ID = 1;
+      const token = service.sign(ID);
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenLastCalledWith({ id: ID }, TEST_KEY);
+    });
   });
 });
