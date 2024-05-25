53925a75dda42d3f4cb2e964e6e2fc2ede70fdd1
leejeawon92 <jeawon33333@gmail.com>
2024-05-25 23:46:14 +0900

    jwt verify test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -3,13 +3,15 @@ import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
 import * as jwt from 'jsonwebtoken';
 
+const TEST_KEY = 'testKey';
+const USER_ID = 1;
+
 jest.mock('jsonwebtoken', () => {
   return {
     sign: jest.fn(() => 'TOKEN'),
+    verify: jest.fn(() => ({ id: USER_ID })),
   };
 });
-
-const TEST_KEY = 'testKey';
 
 describe('JwtService', () => {
   let service: JwtService;
@@ -32,10 +34,19 @@ describe('JwtService', () => {
   describe('sign', () => {
     it('토큰을 반환해야 한다.', () => {
       const ID = 1;
-      const token = service.sign(ID);
+      const token = service.sign(USER_ID);
       expect(typeof token).toBe('string');
       expect(jwt.sign).toHaveBeenCalledTimes(1);
-      expect(jwt.sign).toHaveBeenLastCalledWith({ id: ID }, TEST_KEY);
+      expect(jwt.sign).toHaveBeenLastCalledWith({ id: USER_ID }, TEST_KEY);
     });
   });
+  describe('verify', () => {
+    it('디코딩된 토큰을 반환해야 한다.', () => {
+      const TOKEN = 'TOKEN';
+      const decodedToken = service.verify(TOKEN);
+      expect(decodedToken).toEqual({ id: USER_ID });
+      expect(jwt.verify).toHaveBeenCalledTimes(1);
+      expect(jwt.verify).toHaveBeenCalledWith(TOKEN, TEST_KEY);
+    });
+  })
 });
