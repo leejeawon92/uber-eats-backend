3c0a8982a8fd00f61d41e99e77f982d60c10a808
leejeawon92 <jeawon33333@gmail.com>
2024-07-17 18:13:42 +0900

    create payment service

--- src/payments/payments.resolver.ts
+++ src/payments/payments.resolver.ts
@@ -1,8 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payments.service';
+import { CreatePaymentInput, CreatePaymentOuput } from './dtos/create-payment.dto';
+import { Role } from 'src/auth/role.decorator';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
 @Resolver(of => Payment)
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Mutation(returns => CreatePaymentOuput)
+  @Role(['Owner'])
+  createPayment(
+    @AuthUser() owner: User,
+    @Args('input') createPaymentInput: CreatePaymentInput,
+  ): Promise<CreatePaymentOuput> {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
 }
